72b70b1f07ee0bbb325978e7285d35fcd33e828c
Bugra Yildirim <buggrayldrm@gmail.com>
2023-03-17 11:35:49 +0300

    src

---
+++ CoreDemo/DataAccessLayer/Migrations/20230316075644_mig3.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BlogId",
+                table: "Comment",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_BlogId",
+                table: "Comment",
+                column: "BlogId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comment_Blogs_BlogId",
+                table: "Comment",
+                column: "BlogId",
+                principalTable: "Blogs",
+                principalColumn: "BlogId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_Blogs_BlogId",
+                table: "Comment");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comment_BlogId",
+                table: "Comment");
+
+            migrationBuilder.DropColumn(
+                name: "BlogId",
+                table: "Comment");
+        }
+    }
+}
---
+++ CoreDemo/DataAccessLayer/Repositories/BlogRepository.cs
@@ -0,0 +1,47 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Cocnrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class BlogRepository : IBlogDal
+    {
+
+        public void AddBlog(Blog blog)
+        {
+            using var c = new Context();
+            c.Add(blog);
+            c.SaveChanges();
+        }
+        public void DeleteBLog(Blog blog)
+        {
+            using var c = new Context();
+            c.Remove(blog);
+            c.SaveChanges();
+        }
+
+        public Blog getById(int id)
+        {
+            using var c = new Context();
+            return c.Blogs.Find(id);
+        }
+
+        public List<Blog> LİstAllBlog()
+        {
+            using var c = new Context();
+            return c.Blogs.ToList();
+        }
+
+        public void UpdateBLog(Blog blog)
+        {
+            using var c = new Context();
+            c.Update(blog);
+            c.SaveChanges();
+        }
+    }
+}
